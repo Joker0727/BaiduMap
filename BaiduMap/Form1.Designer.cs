b36d59b9e3d4a56fdd4494cfe403f791986aa1c3
unknown <1610779207@qq.com>
2019-01-04 01:11:47 +0800

    上传百度地图自动评论

---
+++ BaiduMap/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace BaiduMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tabPage2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(424, 144);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "显示面板";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(418, 138);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "名称";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "状态";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(432, 170);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.checkBox2);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(424, 144);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "控制面板";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(364, 37);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 21);
+            this.numericUpDown1.TabIndex = 20;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(305, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "固定张数";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(340, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 18;
+            this.checkBox1.Text = "图片随机";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(220, 21);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 64);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 73);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 21);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "图片文件夹";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "评论内容";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "搜索关键字";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(340, 63);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(60, 16);
+            this.checkBox2.TabIndex = 21;
+            this.checkBox2.Text = "不传图";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 172);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BaiduMap";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ BaiduMap/Form1.cs
@@ -0,0 +1,339 @@
+﻿using MyTools;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tool;
+
+namespace BaiduMap
+{
+    public partial class Form1 : Form
+    {
+        public SeleniumHelper sel = null;
+        public string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        public Thread thread = null;
+        public string mainUrl = "https://map.baidu.com/";
+        public bool isLogin = false;
+        public int page = 0;
+        public int current = 0;
+        public MyUtils myUtils = null;
+        private readonly string workId = "0020";
+        private string searchStr = string.Empty, commentStr = string.Empty, imgFolder = string.Empty;
+        public bool isRandom = false;
+        public List<string> imgPathList = new List<string>();
+        public int fNumber = 0;
+        public bool noPic = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;//屏蔽跨线程操作ui控件的异常
+            sel = new SeleniumHelper();
+            sel.driver.Navigate().GoToUrl(mainUrl);
+            if (sel.driver.WindowHandles.Count() > 1)
+            {
+                sel.driver.SwitchTo().Window(sel.driver.WindowHandles[1]);
+                sel.driver.Close();
+                sel.driver.SwitchTo().Window(sel.driver.WindowHandles[0]);
+            }
+            MessageBoxButtons message = MessageBoxButtons.OKCancel;
+            DialogResult dr = MessageBox.Show("请先登录成功后，再点击确定！", "BaiduMap", message);
+            if (dr == DialogResult.OK)
+                isLogin = true;
+            else
+                isLogin = false;
+            myUtils = new MyUtils();
+            this.MaximizeBox = false;
+            noPic = this.checkBox2.Checked;
+        }
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.textBox3.Text = path.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string btnStr = this.button1.Text;
+            searchStr = this.textBox1.Text;
+            commentStr = this.textBox2.Text;
+            imgFolder = this.textBox3.Text;
+            isRandom = this.checkBox1.Checked;
+            fNumber = Convert.ToInt32(this.numericUpDown1.Text);
+            if (string.IsNullOrEmpty(searchStr))
+            {
+                MessageBox.Show("搜索关键词词和评论不能为空！", "BaiduMap");
+                return;
+            }
+            if (string.IsNullOrEmpty(imgFolder))
+            {
+                MessageBox.Show("图片路径不能为空！", "BaiduMap");
+                return;
+            }
+            if (!Directory.Exists(imgFolder))
+            {
+                MessageBox.Show("图片路径不存在！", "BaiduMap");
+                return;
+            }
+            if (commentStr.Length < 15)
+            {
+                MessageBox.Show("评论内容不能小于15个字！", "BaiduMap");
+                return;
+            }
+            if (IsAuthorised())
+            {
+                MessageBox.Show("尚未授权！", "BaiduMap");
+                return;
+            }
+            if (!isLogin)
+            {
+                MessageBox.Show("请退出程序重新你登陆！", "BaiduMap");
+                return;
+            }
+            if (btnStr == "开始")
+            {
+                this.button1.Text = "暂停";
+                if (thread == null)
+                {
+                    imgPathList = myUtils.GetImgs(imgFolder, 2);
+                    thread = new Thread(StartWork);
+                    thread.IsBackground = true;
+                    thread.Start();
+                }
+                else
+                {
+                    thread.Resume();
+                }
+            }
+            else
+            {
+                this.button1.Text = "开始";
+                thread?.Suspend();
+            }
+        }
+
+        public void StartWork()
+        {
+            page = 1;
+            current = 0;
+            bool isRefresh = false;
+            string shopName = string.Empty;
+            ListViewItem lt = null;
+            GETSHOPLIST:
+            {
+                if (current >= 10)
+                {
+                    page++;
+                    current = 0;
+                }
+                IWebElement soleInput = sel.FindElementById("sole-input");
+                soleInput.Clear();
+                soleInput.SendKeys(searchStr);
+                Thread.Sleep(1000 * 2);
+
+                IWebElement searchButton = sel.FindElementById("search-button");
+                //移动光标到指定的元素上perform
+                Actions action = new Actions(sel.driver);
+                action.MoveToElement(searchButton).Perform();
+                searchButton.Click();
+                Thread.Sleep(1000 * 2);
+                bool isOk = false;
+                while (!isOk)
+                {
+                    IWebElement curPageNode = sel.FindElementByClassName("curPage");
+                    if (Convert.ToInt32(curPageNode.Text) == page) break;
+                    var spanNodeList = sel.FindElementsByXPath("//div[@id='poi_page']/p[@class='page']/span");
+                    foreach (var nextNode in spanNodeList)
+                    {
+                        if (Regex.IsMatch(nextNode.Text, @"^[+-]?\d*[.]?\d*$"))
+                        {
+                            int pageNumber = Convert.ToInt32(nextNode.Text);
+                            if (pageNumber == page)
+                            {
+                                nextNode.Click();
+                                isOk = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!isOk && spanNodeList.Count > 0)
+                    {
+                        spanNodeList[spanNodeList.Count - 1].Click();
+                    }
+                }
+            }
+
+            Thread.Sleep(1000 * 2);
+            var shopNodeList = sel.FindElementsByCss(".search-item.base-item");
+            try
+            {
+                IWebElement shopNameNode = shopNodeList[current].FindElement(By.ClassName("n-blue"));
+                shopName = shopNameNode?.Text;
+                shopNodeList[current].Click();
+                Thread.Sleep(1000 * 2);
+                IWebElement writeTextNode = sel.FindElementByClassName("write-btn-text");
+                writeTextNode.Click();
+                Thread.Sleep(1000 * 1);
+                string newWindows = sel.driver.WindowHandles[1];
+                sel.driver.SwitchTo().Window(newWindows);
+                Thread.Sleep(1000 * 2);
+
+                REFRESH:
+                {
+                    if (isRefresh)
+                    {
+                        ((IJavaScriptExecutor)sel.driver).ExecuteScript("location.reload()");//刷新
+                        Thread.Sleep(1000 * 2);
+                        isRefresh = false;
+                    }
+                }
+                var startNodeList = sel.FindElementsByXPath("//ul[@id='data-star']/li");
+                if (startNodeList != null)
+                {
+                    if (startNodeList.Count() == 5)
+                        startNodeList[4].Click();
+                }
+                else
+                {
+                    isRefresh = true;
+                    goto REFRESH;
+                }
+                var facilitiesNodeList = sel.FindElementsByXPath("//ul[@id='7']/li");
+                if (facilitiesNodeList != null)
+                {
+                    if (facilitiesNodeList.Count() == 5)
+                        facilitiesNodeList[4].Click();
+                }
+                var environmentalNodeList = sel.FindElementsByXPath("//ul[@id='8']/li");
+                if (environmentalNodeList != null)
+                {
+                    if (environmentalNodeList.Count() == 5)
+                        environmentalNodeList[4].Click();
+                }
+                var servicesNodeList = sel.FindElementsByXPath("//ul[@id='9']/li");
+                if (servicesNodeList != null)
+                {
+                    if (servicesNodeList.Count() == 5)
+                        servicesNodeList[4].Click();
+                }
+                IWebElement commentInputNode = sel.FindElementById("remark-text");
+                if (commentInputNode != null)
+                {
+                    commentInputNode.SendKeys(commentStr);
+                    //上传图片                   
+                    if (!noPic)
+                        UpLoadPic();
+                    IWebElement submitBtnNode = sel.FindElementByCss(".submit-btn.state-finish");
+                    if (submitBtnNode == null)
+                        submitBtnNode = sel.FindElementByCss(".submit-btn.disabled.state-pedding");
+                    submitBtnNode.Click();
+                }
+                Thread.Sleep(1000 * 2);
+                sel.driver.Close();
+                string oldWindows = sel.driver.WindowHandles[0];
+                sel.driver.SwitchTo().Window(oldWindows);
+                current++;
+                lt = new ListViewItem();
+                lt.Text = shopName;
+                lt.SubItems.Add("发布成功");
+                this.listView1.Items.Add(lt);
+                Thread.Sleep(1000 * 2);
+                goto GETSHOPLIST;
+            }
+            catch (Exception ex)
+            {
+                current++;
+                lt = new ListViewItem();
+                lt.Text = shopName;
+                lt.SubItems.Add("发布失败");
+                this.listView1.Items.Add(lt);
+                myUtils.WriteLog(ex);
+                goto GETSHOPLIST;
+            }
+        }
+
+        public void UpLoadPic()
+        {
+            int count = 0;
+            if (isRandom)
+                count = GetRandom();
+            else
+                count = fNumber > 0 ? fNumber : 9;
+            string tempPath = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    tempPath = imgPathList[i];
+                    if (!File.Exists(tempPath))
+                        continue;
+                    // 上传图片
+                    IWebElement addPicBtnNode = sel.FindElementByName("file");
+                    addPicBtnNode.SendKeys(tempPath);
+                    Thread.Sleep(1000 * 2);
+                }
+                catch (Exception ex)
+                {
+                    myUtils.WriteLog("图片上传失败：" + ex);
+                }
+            }
+        }
+        public int GetRandom()
+        {
+            int imgCount = imgPathList.Count();
+            Random randObj = new Random();
+            int start = 1;//随机数可取该下界值
+            int end = imgCount + 1;//随机数不能取该上界值
+            int random = randObj.Next(start, end);
+            return random;
+        }
+
+        /// <summary>
+        /// 授权
+        /// </summary>
+        /// <param name="workId"></param>
+        /// <returns></returns>
+        public bool IsAuthorised()
+        {
+            string conStr = "Server=111.230.149.80;DataBase=MyDB;uid=sa;pwd=1add1&one";
+            bool bo = false;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    string sql = string.Format("select count(*) from MyWork Where IsAuth = 1 and WorkId ='{0}'", workId);
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        con.Open();
+                        int count = int.Parse(cmd.ExecuteScalar().ToString());
+                        if (count > 0)
+                            bo = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                bo = false;
+            }
+
+            return bo;
+        }
+    }
+}
